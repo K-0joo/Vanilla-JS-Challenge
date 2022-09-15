8fd7f3deb414edbe1cc5d50d09d222302a147608
K-0joo <tyty4420@naver.com>
2022-09-15 15:02:10 +0900

    4.2 Events

--- js/app.js
+++ js/app.js
@@ -1,15 +1,11 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function handleLoginBtnClick(){
-    const username = loginInput.value;
-    // if (username === "") {
-    //     alert("Please write your name");
-    
-    // } else if (username.length > 15) {
-    //     alert("Your name is too long.");
-    // }
-    console.log(username);
+function onLoginSubmit(event){
+    event.preventDefault();
+    console.log(loginInput.value);
+    // const username = loginInput.value;
+    // console.log(username);
 }
 
-loginButton.addEventListener("click", handleLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
